ecdef399d7a70629db7398681dbd04dbd10c8b47
Lucas Dias <DiasLucP@gmail.com>
2025-10-07 15:21:49 -0400

    Adding screenshots

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,14 +1,17 @@
 describe('Login spec', () => {
   beforeEach(() => {
     cy.visit('http://localhost:4000')
+    cy.screenshot('after-visiting-page')
   })
 
-  it('Login with valid credentials should allow system entry', () => {
+  it.only('Login with valid credentials should allow system entry', () => {
     
     cy.get('#username').click().type('julio.lima')
     cy.get('#senha').click().type('123456')
+    cy.screenshot('after-filling-valid-credentials')
     // Initial way of getting button
     cy.get('#login-section > .btn').click()
+    cy.screenshot('after-clicking-enter-button')
 
     cy.contains('h4', 'Realizar Transferência').should('be.visible')
   })
